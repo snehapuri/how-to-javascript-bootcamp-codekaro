60ebd86ba4ea3791527a77c5bd400f62bd07936a
Sneha Puri <snehapuri484@gmail.com>
2024-01-15 23:56:38 +0530

    Day 2 Assignments 1,2,3,4

---
+++ Day2/app.js
@@ -0,0 +1,67 @@
+// Assignment 1: JS function to take number as parameter and print if positive, negative or zero
+
+function check (num) {
+    if (num>0) {
+        console.log('positive')
+    }
+    else if (num<0) {
+        console.log('is negative')
+    }
+    else {
+        console.log('zero')
+    }
+}
+check(9)
+check(-2)
+check(0)
+
+
+// Assignment 2: JS function that takes a positive integer as a parameter and calculates it's factorial using for loop.
+
+function fact (n) {
+    fact = 1
+    for (let i=1;i<=n;i++) {
+        fact = fact*i 
+    }
+    console.log(fact)
+}
+fact(4)
+
+
+// Assignment 3: JS function that takes two numbers as parameters and returns the larger 
+
+function greater (n1,n2) {
+    let str = "neither"
+    if (n1>n2) {
+        return n1
+    }
+    else if (n2>n1) {
+        return n2
+    }
+    else {
+        return str
+    }
+}
+console.log(greater(-12,4)," is greater")
+console.log(greater(7,7)," is greater")
+
+
+// Assignment 4: JS function that takes a string as a parameter and checks if it is palindrome. 
+// A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+function palindrome(str) {
+    str = str.split(" ").join("").toLowerCase();
+    let reverse = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
+    console.log(reverse)
+    if (str == reverse) {
+        console.log("Palindrome");
+    } 
+    else {
+        console.log("Not Palindrome");
+    }
+}
+palindrome("Evil OliVe");
+palindrome("Hello");
